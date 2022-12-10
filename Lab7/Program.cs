e1d49a9d8f3b026d8430ac0042de1ea851c97482
User <User@DESKTOP-RPSJ6FD>
2022-12-10 10:56:00 +0200

    Add project files.

---
+++ Lab7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat barsic = new Cat(5, "Brown", 2);
+            Dog knopa = new Dog(2, "White", 1);
+
+            Console.WriteLine(barsic.ToString());
+            Console.WriteLine(barsic.MakeSound());
+            Console.WriteLine(knopa.ToString());
+            Console.WriteLine(knopa.MakeSound());
+
+            Console.ReadKey();
+
+        }
+    }
+}
